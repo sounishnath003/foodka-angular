aa13e5bc388e8f48e6927efcd657bf7e78453f49
Sounish Nath <flock.sinasini@gmail.com>
2020-03-04 01:01:46 +0530

    snack bar addded google

--- foodka/src/app/signin/signin.component.ts
+++ foodka/src/app/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: "app-signin",
@@ -11,7 +12,7 @@ export class SigninComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(public _formBuilder: FormBuilder) {}
+  constructor(public _formBuilder: FormBuilder, private _snackbar: MatSnackBar) {}
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -21,4 +22,19 @@ export class SigninComponent implements OnInit {
       secondCtrl: ["", Validators.required, Validators.email]
     });
   }
+
+  open//import {MatSnackBar} from '@angular/material';
+  //constructor(public snackBar: MatSnackBar) {}
+  openSnackBar(message: string, action: string) {
+    this._snackbar.open(message, action, {
+      duration: 1500,
+      panelClass: 'blue'
+    });
+  }
+
+  onSubmit = () => {
+    this.openSnackBar("You'll be successfully logged in ", "UNDO")
+  }
+
+
 }
