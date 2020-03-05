3981c36bfc8986b9b0310a352a2cfed9f2bda361
Sounish Nath <flock.sinasini@gmail.com>
2020-03-05 22:37:08 +0530

    switching nav components FIXED 🙌🙌

--- foodka/src/app/resturants/resturant-details/resturant-details.component.ts
+++ foodka/src/app/resturants/resturant-details/resturant-details.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ResturantsDetailsService } from '../../services/resturants-details.service';
+import { Component, OnInit } from "@angular/core";
+import { ResturantsDetailsService } from "../../services/resturants-details.service";
 
 @Component({
-  selector: 'app-resturant-details',
-  templateUrl: './resturant-details.component.html',
-  styleUrls: ['./resturant-details.component.css']
+  selector: "app-resturant-details",
+  templateUrl: "./resturant-details.component.html",
+  styleUrls: ["./resturant-details.component.css"]
 })
 export class ResturantDetailsComponent implements OnInit {
-  data:any={};
-  constructor(private _resturantData:ResturantsDetailsService) { 
+  data: any = {};
+  constructor(private _resturantData: ResturantsDetailsService) {
     this.data = this._resturantData.getResturantsData()[0];
-    console.log(this.data)
+    console.log(this.data);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
