6c051b1e31614ab5f8989118efcc34ddbe1d91da
SAYAN MAITY <sayanmaity007@gmail.com>
2020-03-07 16:08:43 +0530

    restaurant details route by id fix

--- foodka/src/app/resturants/resturant-details/nonVegFoodCard/nonVegFoodCard.component.ts
+++ foodka/src/app/resturants/resturant-details/nonVegFoodCard/nonVegFoodCard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ResturantsDetailsService } from 'src/app/services/resturants-details.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: "app-nonVegFoodCard",
@@ -7,10 +8,15 @@ import { ResturantsDetailsService } from 'src/app/services/resturants-details.se
   styleUrls: ["./nonVegFoodCard.component.css"]
 })
 export class NonVegFoodCardComponent implements OnInit {
-  
   data: any = {};
   nonVegFood: any = {};
-  constructor(private _foodData: ResturantsDetailsService) {
+  constructor(
+    private _foodData: ResturantsDetailsService,
+    private _ActivatedRoute: ActivatedRoute
+  ) {
+    this._ActivatedRoute.params.subscribe(param=>{
+      // console.log(param)
+    })
     this.data = this._foodData.getResturantsData()[0];
     this.nonVegFood = this.data.foods.nonveg;
   }
--- foodka/src/app/resturants/resturant-details/resturant-details.component.ts
+++ foodka/src/app/resturants/resturant-details/resturant-details.component.ts
@@ -13,37 +13,43 @@ export class ResturantDetailsComponent implements OnInit {
   data: any = {};
   resturantID: number;
   allResturants: {};
-
+  id:number;
   constructor(
     private _resturantData: ResturantsDetailsService,
     private _openDialog: MatDialog,
     private _activeRouter: ActivatedRoute
   ) {
+    this._activeRouter.params.subscribe(param=>{
+      this.id = param.id;
+      console.log(this.id)
+    })
+    this.data = this._resturantData.getResturantsData()[--this.id]
+    console.log(this.data)
   }
 
   ngOnInit(): void {
-    this._microServices() ;
+    // this._microServices() ;
   }
 
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.data = undefined;
-    this.data = {};
-  }
+  // ngOnDestroy(): void {
+  //   //Called once, before the instance is destroyed.
+  //   //Add 'implements OnDestroy' to the class.
+  //   this.data = undefined;
+  //   this.data = {};
+  // }
 
 
-  _microServices = () => {
-    this.allResturants = this._resturantData.getResturantsData();
-    this._activeRouter.params.subscribe(res => (this.resturantID = res.id));
-    console.log(this.resturantID);
-    try {
-      this.data = this._resturantData.getResturantById(this.resturantID);
-      // console.log(this.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // _microServices = () => {
+  //   this.allResturants = this._resturantData.getResturantsData();
+  //   this._activeRouter.params.subscribe(res => (this.resturantID = res.id));
+  //   console.log(this.resturantID);
+  //   try {
+  //     this.data = this._resturantData.getResturantById(this.resturantID);
+  //     // console.log(this.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
 
   openOfferDeatails() {
